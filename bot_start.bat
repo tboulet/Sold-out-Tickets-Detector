a5803848bae312df9bc6ee24a1475eebdb202e24
Timothé Boulet <timothe.boulet@ubisoft.com>
2023-06-21 14:24:40 -0400

    update

--- bot_start.bat
+++ bot_start.bat
@@ -1,2 +1,11 @@
-venv\Scripts\activate
-cmd /k py run.py
+@echo off
+
+REM Activate the virtual environment
+call venv\Scripts\activate
+
+REM Execute the Python script
+python run.py
+timeout /t 5
+
+REM Deactivate the virtual environment
+deactivate
